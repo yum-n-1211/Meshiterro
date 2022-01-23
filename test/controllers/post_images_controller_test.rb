2d398df3913df6c9bf4f72ab55cf4d7ad7e48af5
Your Name <you@example.com>
2022-01-23 08:44:24 +0000

    [Finish]4-10

--- test/controllers/post_images_controller_test.rb
+++ test/controllers/post_images_controller_test.rb
@@ -1,18 +1,18 @@
 require "test_helper"
 
-class PostImageControllerTest < ActionDispatch::IntegrationTest
+class PostImagesControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get post_image_new_url
+    get post_images_new_url
     assert_response :success
   end
 
   test "should get index" do
-    get post_image_index_url
+    get post_images_index_url
     assert_response :success
   end
 
   test "should get show" do
-    get post_image_show_url
+    get post_images_show_url
     assert_response :success
   end
 end
