c04201820ee88a707ddc93d6ef55b6017b56dd54
Your Name <you@example.com>
2022-01-24 13:36:23 +0000

    [Finish]4-14

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  
+  has_many :post_images, dependent: :destroy
+  has_one_attached :profile_image
+
+  def get_profile_image(size)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize: size).processed
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  root 'homes#top'
+  root to:'homes#top'
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
