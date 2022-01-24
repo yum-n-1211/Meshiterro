e3a656cd493f0006c414ef0340d8a4fd5d43b996
Your Name <you@example.com>
2022-01-24 14:30:25 +0000

    [Finish]4-16

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to:'homes#top'
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 
 end
