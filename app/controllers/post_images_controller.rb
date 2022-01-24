cfcd25b68ad776ae7ae8a3c8f4fe25ac63545e7b
Your Name <you@example.com>
2022-01-24 07:04:22 +0000

    [Finish]4-12

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -12,10 +12,11 @@ class PostImagesController < ApplicationController
 
 
   def index
-     @post_images =PostImage.all
+    @post_images = PostImage.all
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
 
 # 投稿データのストロングパラメータ
