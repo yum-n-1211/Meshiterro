e8ae9533c3df31987df6ea6765adba3720064d37
Your Name <you@example.com>
2022-01-25 11:42:43 +0000

    [Finish]4-19

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id)  
+    redirect_to user_path(@user.id) 
   end
 
 
